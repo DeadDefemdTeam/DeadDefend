97375d8cc3a61e264bd1d68405ee1d7f37500083
YingRs <1335738607@qq.com>
2025-05-07 14:57:10 +0800

    基础框架提交

--- src/Script/OutputCfg/TableData.lua
+++ src/Script/OutputCfg/TableData.lua
@@ -1,7 +1,7 @@
 -- 表格数据索引模块
 local TableData = {
-    Enemy = require(script.Enemy),
-    Item = require(script.Item),
+    Enemy = require(script.Parent.Enemy),
+    Item = require(script.Parent.Item),
 }
 
 return TableData
